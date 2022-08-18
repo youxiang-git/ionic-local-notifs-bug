d57a255db761fe054b3468f718c21580db1fb981
C Youxiang <68730868+youxiang-git@users.noreply.github.com>
2022-08-18 11:04:02 +0800

    initial commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,42 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { LocalNotifications } from '@capacitor/local-notifications';
 import './Home.css';
+import { useEffect } from 'react';
 
 const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
-  );
+	const scheduleNotifs = async () => {
+		await LocalNotifications.requestPermissions();
+		await LocalNotifications.schedule({
+			notifications: [
+				{
+					title: 'LocalNotifications test',
+					body: 'ScheduleOn bug test',
+					id: 1,
+					schedule: {
+						on: {
+							second: 0,
+						},
+						allowWhileIdle: true,
+					},
+				},
+			],
+		});
+		console.log('Notifications scheduled');
+	};
+
+	useEffect(() => {
+		scheduleNotifs();
+	}, []);
+
+	return (
+		<IonPage>
+			<IonHeader>
+				<IonToolbar>
+					<IonTitle>Local Notifications</IonTitle>
+				</IonToolbar>
+			</IonHeader>
+		</IonPage>
+	);
 };
 
 export default Home;
